4ac4524a516d094feb24196b6da2f62a198d7dd8
Linus Fessler <linusfessler@gmail.com>
2019-01-26 12:18:19 +0100

    Modelle + Destructible

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+    
+    [SerializeField] private ParticleSystem destructionEffectPrefab;
+    //[SerializeField] private AudioClip destructionSound;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Floor")) {
+            ParticleSystem destructionEffect = Instantiate<ParticleSystem>(destructionEffectPrefab);
+            destructionEffect.transform.position = collision.GetContact(0).point;
+            destructionEffect.Play();
+            Destroy(destructionEffect, destructionEffect.main.duration);
+
+            // GameObject audio = new GameObject();
+
+
+            Destroy(gameObject);
+        }
+    }
+}
